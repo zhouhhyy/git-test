abdb72f1bb4e52914f16eb1ec7a76c2bec417561
zhouhhyy <155034515+zhouhhyy@users.noreply.github.com>
2024-02-26 09:28:11 +0800

    Update hello.java
    远程仓库提交

--- src/main/java/com/zhy/demo/hello.java
+++ src/main/java/com/zhy/demo/hello.java
@@ -8,5 +8,6 @@ public class hello {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("remote test");
     }
 }
